5b09c9bb54d6c259ff408e8c73048593e1b30ce5
Andriikryl <andreyk557@gmail.com>
2023-11-14 15:13:34 +0200

    add FlexExempleOne component

--- src/components/flexExemple/FlexExempleOne/FlexExempleOne.tsx
+++ src/components/flexExemple/FlexExempleOne/FlexExempleOne.tsx
@@ -9,6 +9,19 @@ export default function FlexExempleOne() {
         <div className={style.flex__item}></div>
         <div className={style.flex__item}></div>
       </div>
+      <div className={style.code__box}>
+        <code className={style.code}>
+          <span>.layout&#123;</span>
+          <span>display: flex;</span>
+          <span>gap: 20px;</span>
+          <span>flex-wrap: wrap;</span>
+          <span>max-width: 500px;</span>
+          &#125;
+          <span>.flex__item:nth-child(3)&#123;</span>
+          <span>flex-grow:1</span>
+          <span> &#125;</span>
+        </code>
+      </div>
     </div>
   );
 }
